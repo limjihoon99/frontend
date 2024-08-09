b7ca1ef3ea9a316928f83cbd6ddb6b04297b731b
limjihoon99 <bo9701@naver.com>
2024-08-09 17:41:30 +0900

    refactor: Update 05_BOM file

--- 03_JavaScript/05_BOM.js
+++ 03_JavaScript/05_BOM.js
@@ -12,4 +12,39 @@ window.onload = () => {
         window.open('https://www.naver.com', 'naver', 'width=500', 'height=400');
     });
 
+    // 2) window.setTimeout()
+    let btn2 = document.getElementById('btn2');
+
+    btn2.addEventListener('click', () => {
+        let timerId = 0;
+        let newWindow = window.open();
+
+        newWindow.alert('이 페이지는 3초 후에 종료됩니다.');
+
+        // 일정 시간 후에 콜백 함수를 한 번 실행 (3초 후에 동작)
+        timerId = window.setTimeout(() => newWindow.close(), 3000);
+
+        // 타이머 ID를 clearTimeout()에 전달하면 해당 ID의 타이머를 취소할 수 있다.
+        window.clearTimeout(timerId);
+    });
+
+    // 3) window.setInterval()
+    let timerId = 0;
+    let btnStart = document.getElementById('btnStart');
+    let btnStop = document.getElementById('btnStop');
+
+    btnStart.addEventListener('click', () => {
+        let div1 = document.getElementById('div1');
+
+        timerId = window.setInterval(() => {
+            let now = new Date();
+            
+            div1.innerHTML = `${now.getHours()} : ${now.getMinutes()} : ${now.getSeconds()}`;
+        }, 1000);
+    });
+
+    btnStop.addEventListener('click', () => {
+        window.clearInterval(timerId);
+    });
+
 };
